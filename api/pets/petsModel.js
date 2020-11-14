4c664c1297c2ead6529f41d7b9a2bec4566e23b5
Shadewalker1984 <Shadewalker1984@yahoo.com>
2020-11-13 19:39:19 -0600

    changed the model to better represent a specific customers dataset

--- api/pets/petsModel.js
+++ api/pets/petsModel.js
@@ -1,24 +1,25 @@
 /* eslint-disable prettier/prettier */
 const db = require('../../data/db-config');
 
-const getAll = async () => {
-  return await db('pets');
+const getAll = async (cust_id) => {
+  return await db('pets').where('customer_id', cust_id).select('*');
 };
 
-const getById = async (id) => {
-  return await db('pets').where('id', id).first().select('*');
+// This one chooses the pet by id, where the customer ID matches and the ID matches the required id
+const getById = async (customer_id, id) => {
+  return await db('pets').where({customer_id: customer_id, id: id}).first().select('*');
 };
 
 const create = async (data) => {
   return await db('pets').insert(data).returning('*')
 };
 
-const update = async (id, data) => {
-  return await db('pets').where('id', id).first().update(data).returning('*');
+const update = async (customer_id, id, data) => {
+  return await db('pets').where({customer_id: customer_id, id: id}).first().update(data).returning('*');
 };
 
-const remove = async (id) => {
-  return await db('pets').where('id', id).del();
+const remove = async (customer_id, id) => {
+  return await db('pets').where({customer_id: customer_id, id: id}).del();
 };
 
 module.exports = {
