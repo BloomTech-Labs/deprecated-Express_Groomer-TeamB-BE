d8c3353fe1ef10d98c46714e6e10fcfa74877323
Jessica Cooper <jessicagcooper@gmail.com>
2020-11-13 20:40:13 -0600

    Whitelisting all in profile router.

--- api/profile/profileRouter.js
+++ api/profile/profileRouter.js
@@ -4,7 +4,7 @@ const Profiles = require('./profileModel');
 const router = express.Router();
 
 router.all('/', function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'https://b.expressgroomer.dev');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   next();
 });
